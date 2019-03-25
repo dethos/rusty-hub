f66a16c34c9054910c470644ca5327bc2d3a1e1d
Gonçalo Valério <gon@ovalerio.net>
2019-03-25 23:10:36 +0000

    added testing module to the controllers

--- src/controllers.rs
+++ src/controllers.rs
@@ -3,3 +3,15 @@ use actix_web::{HttpRequest, Responder};
 pub fn index(_req: &HttpRequest) -> impl Responder {
     "Hello world!"
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http, test};
+
+    #[test]
+    fn test_index() {
+        let resp = test::TestRequest::default().run(&index).unwrap();
+        assert_eq!(resp.status(), http::StatusCode::OK);
+    }
+}
